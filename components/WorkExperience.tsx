9f1c7d7c5caa28d39cb0cacce47e43a4cd91db9b
BlazerXXI <ruslandendik@gmail.com>
2023-02-01 21:49:17 +0200

    dev exp

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const WorkExperience = () => {
   return (
-    <div className=" h-screen">
-      <h3 className=" absolute top-24 uppercase overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto">
+    <div className=" h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+      <h3 className=" absolute top-24 uppercase  tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
     </div>

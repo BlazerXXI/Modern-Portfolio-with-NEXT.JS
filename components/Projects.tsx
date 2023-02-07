1f6002e7d5cbc4b31bad834523ccc07895d88dcf
BlazerXXI <ruslandendik@gmail.com>
2023-02-07 20:17:45 +0200

    dev

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+import { type } from "os";
 import React from "react";
 
-export const Projects = () => {
-  return <div>Projects</div>;
+type Props = {};
+
+export const Projects = ({}: Props) => {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <div className=" h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className=" absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+      <div className=" relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          // eslint-disable-next-line react/jsx-key
+          <div className=" w-screen flex-shrink-0 snap-center flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <img src="https://picsum.photos/400/200" alt="" />
+            <div>
+              <h4>
+                Case Study {i + 1} of {projects.length}: UPS clone
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className=" w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px]  -skew-y-12"></div>
+    </div>
+  );
 };

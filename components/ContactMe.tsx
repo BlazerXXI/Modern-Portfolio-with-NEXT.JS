f715c44fa3540044abdebc9511315fcbba759df5
BlazerXXI <ruslandendik@gmail.com>
2023-02-10 21:19:18 +0200

    fix

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -23,22 +23,38 @@ export const ContactMe = () => {
         >
           <ChevronDoubleDownIcon className="text-[#f7ab0a] h-28 w-h-28 animate-pulse" />
         </motion.div>
-        <h4 className=" text-4xl font-semibold text-center">
+        <h4 className="flex flex-col text-4xl font-semibold text-center ">
           I have got just what you need.
-          <span className=" decoration-[#f7ab0a]/50 underline">Lets Talk.</span>
+          <a
+            href="mailto:ruslandendik@gmail.com"
+            className=" decoration-[#f7ab0a]/50 underline"
+          >
+            Lets Talk.
+          </a>
         </h4>
         <div className=" space-y-10">
           <div className=" flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p className=" text-2xl">+380990849108</p>
+            <a href="tel:380990849108" className=" text-2xl">
+              +38-099-084-91-08
+            </a>
           </div>
           <div className=" flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p className=" text-2xl">ruslandendik@gmail.com</p>
+            <a href="mailto:ruslandendik@gmail.com" className=" text-2xl">
+              ruslandendik@gmail.com
+            </a>
           </div>
           <div className=" flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p className=" text-2xl">123 Developer Lane</p>
+            <a
+              href="https://goo.gl/maps/K219nTuTQxDrwJtv6"
+              target="blank"
+              rel="noopener norefener nofollow"
+              className=" text-2xl"
+            >
+              123 Developer Lane
+            </a>
           </div>
         </div>
       </div>

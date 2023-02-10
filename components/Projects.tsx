2ba4bbd23270be470a6261aa37d0732289b7df63
BlazerXXI <ruslandendik@gmail.com>
2023-02-10 21:31:58 +0200

    fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import { easeInOut, motion } from "framer-motion";
 import React from "react";
+import css from "styled-jsx/css";
 
 type Props = {};
 
 export const Projects = ({}: Props) => {
-  const projects = [1, 2, 3, 4, 5];
+  const projects = [1, 2, 3, 4, 5, 6];
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -22,30 +23,37 @@ export const Projects = ({}: Props) => {
           <div className=" mt-16 w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
             <motion.img
               initial={{
-                y: -300,
+                y: -60,
                 opacity: 0,
               }}
-              transition={{ duration: 1.3, ease: easeInOut }}
+              transition={{ duration: 1, ease: easeInOut }}
               whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
               className=" max-w-[200px] sm:max-w-[400px]"
               src="https://img001.prntscr.com/file/img001/Y01lTDkNSYihCulTrZgoZQ.png"
               alt=""
             />
-            <div className=" space-y-10 px-0 md:px-10 max-w-6xl">
+            <motion.div
+              initial={{
+                y: -60,
+                opacity: 0,
+              }}
+              transition={{ duration: 1, ease: easeInOut }}
+              whileInView={{ opacity: 1, y: 0 }}
+              className=" space-y-10 px-0 md:px-10 max-w-6xl"
+            >
               <h4 className=" text-4xl font-semibold text-center ">
                 <span className=" underline decoration-[#f7ab0a]/50">
                   Case Study {i + 1} of {projects.length}
                 </span>
-                : UPS clone
+                : WebSite
               </h4>
-              <p className=" text-lg text-center md:text-left">
+              <p className=" text-lg text-center text-[#cecece]">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Corrupti illo aperiam reiciendis? Magni nemo non nisi, quidem
                 eius et inventore odio ea ullam molestias nihil hic velit! Quasi
                 distinctio temporibus reprehenderit delectus animi.
               </p>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>

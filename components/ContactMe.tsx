2337e03c17c0d032f567d24db8d603e2488ad48a
BlazerXXI <ruslandendik@gmail.com>
2023-02-17 19:42:36 +0200

    dev

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -6,8 +6,16 @@ import {
   ChevronDoubleDownIcon,
 } from "@heroicons/react/24/solid";
 import { easeInOut, motion } from "framer-motion";
+import { useForm } from "react-hook-form";
 
 export const ContactMe = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (formData: any) => console.log(formData);
   return (
     <div className=" h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center">
       <h3 className=" absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -57,6 +65,42 @@ export const ContactMe = () => {
             </a>
           </div>
         </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className=" flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className=" flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className=" contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className=" contactInput"
+              type="email"
+            />
+          </div>
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className=" contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className=" contactInput"
+          />
+          <button
+            type="submit"
+            className=" bg-[#f7ab0a] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );

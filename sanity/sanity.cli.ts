ab1d4a201a610a16dee58fc914dddc47b1e0c031
BlazerXXI <ruslandendik@gmail.com>
2023-02-27 19:09:23 +0200

    fixed cli

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 's2hzd3aj',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })

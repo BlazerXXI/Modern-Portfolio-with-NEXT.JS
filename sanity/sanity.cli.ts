7d80f0069e6c3ad0397f71155e95c07a1322b574
BlazerXXI <ruslandendik@gmail.com>
2023-02-27 23:03:34 +0200

    ff

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,11 @@
-import {defineCliConfig} from 'sanity/cli'
+// import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: '0m7mplbs',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
+function defineCliConfig(arg0: {api: {projectId: string; dataset: string}}) {
+  throw new Error('Function not implemented.')
+}

2dea8d69cd7cb003ff62849e298621e768fcca84
BlazerXXI <ruslandendik@gmail.com>
2023-02-28 23:02:00 +0200

    dev Skills section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,6 +2,8 @@ import { motion } from "framer-motion";
 import React from "react";
 
 export const Skill = () => {
+  const imgSkill =
+    "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K";
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
@@ -10,7 +12,7 @@ export const Skill = () => {
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1 }}
-        src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
+        src={imgSkill}
         alt=""
         className=" rounded-full border border-gray-500 object-cover w-14 h-14 sm:w-24  sm:h-24 md:w-28  md:h-28 xl:w-32 xl:h-32  transition duration-300 ease-in-out"
       />

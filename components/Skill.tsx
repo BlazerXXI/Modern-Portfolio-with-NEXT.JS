26b9cb9a3916ccfae52117c72fbbcfb1bd31bb33
BlazerXXI <ruslandendik@gmail.com>
2023-03-04 20:29:47 +0200

    dev 04-03-2023-20-29

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -3,10 +3,11 @@ import { motion } from "framer-motion";
 import { SkillImg } from "./SkillImg";
 
 export const Skill = () => {
+
   return (
     <div className="group relative flex cursor-pointer">
       <div className=" absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out group-hover:bg-white w-14 h-14 sm:h-24 sm:w-24 md:w-28  md:h-28  xl:w-32 xl:h-32 rounded-full z-0"></div>
-      <SkillImg />
+			<SkillImg/>
     </div>
   );
 };
--- components/SkillImg.tsx
+++ components/SkillImg.tsx
@@ -2,28 +2,28 @@ import { motion } from "framer-motion";
 import React from "react";
 
 export const SkillImg = () => {
-  const linksArr = [
+  let linksArr = [
     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/900px-JavaScript-logo.png?20120221235433",
     "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
   ];
-  var linkSrc: any = [];
   for (let i = 0; i < linksArr.length; i++) {
-    linkSrc = linksArr[i];
-    console.log(linkSrc);
+    var link: any = linksArr[i];
   }
+
+  console.log(link);
   return (
     <>
+      {" "}
       <motion.img
         initial={{
           opacity: 0,
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1 }}
-        src={linkSrc}
+        src={link}
         alt=""
         className="rounded-full border border-gray-500 object-cover w-14 h-14 sm:w-24  sm:h-24 md:w-28  md:h-28 xl:w-32 xl:h-32  transition duration-300 ease-in-out"
       />
-      ;
     </>
   );
 };

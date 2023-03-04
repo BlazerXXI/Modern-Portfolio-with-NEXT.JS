eb18cb04f806d98a2f47f74e757645a00186a17e
BlazerXXI <ruslandendik@gmail.com>
2023-03-04 19:56:41 +0200

    dev 03-03-23-19-56

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { SkillImg } from "./SkillImg";
 
-export const Skill = (srcLinks: string | undefined) => {
+export const Skill = () => {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{
-          opacity: 0,
-        }}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1 }}
-        src={srcLinks}
-        alt=""
-        className="rounded-full border border-gray-500 object-cover w-14 h-14 sm:w-24  sm:h-24 md:w-28  md:h-28 xl:w-32 xl:h-32  transition duration-300 ease-in-out"
-      />
       <div className=" absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out group-hover:bg-white w-14 h-14 sm:h-24 sm:w-24 md:w-28  md:h-28  xl:w-32 xl:h-32 rounded-full z-0"></div>
+      <SkillImg />
     </div>
   );
 };

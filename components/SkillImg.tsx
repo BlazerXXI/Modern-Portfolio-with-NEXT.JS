fb085365733fd66cca7ccf49834a9bbafa094751
BlazerXXI <ruslandendik@gmail.com>
2023-03-06 21:46:23 +0200

    add skills

--- components/SkillImg.tsx
+++ components/SkillImg.tsx
@@ -4,11 +4,11 @@ import React from "react";
 export const SkillImg = () => {
   let linksArr = [
     {
-      percent: "35%",
+      percent: "65%",
       link: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/900px-JavaScript-logo.png?20120221235433",
     },
     {
-      percent: "60%",
+      percent: "65%",
       link: "https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png",
     },
     {
@@ -23,8 +23,23 @@ export const SkillImg = () => {
       percent: "65%",
       link: "https://cdn.iconscout.com/icon/free/png-256/npm-3521612-2945056.png",
     },
+    {
+      percent: "30%",
+      link: "https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png",
+    },
+    {
+      percent: "80%",
+      link: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png",
+    },
+    {
+      percent: "40%",
+      link: "https://e7.pngegg.com/pngimages/525/356/png-clipart-logo-drupal-content-management-system-gnu-general-public-license-logo-wordpress-text-logo.png",
+    },
+    {
+      percent: "40%",
+      link: "https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png",
+    },
   ];
-  console.log();
   const link = linksArr.map((imgLink) => (
     <li key={linksArr.length} className=" list-none relative">
       <div className=" absolute text-center text-black font-extrabold items-center flex justify-center opacity-0 hover:opacity-60 transition duration-300 ease-in-out hover:bg-white w-14 h-14 sm:h-24 sm:w-24 md:w-28  md:h-28  xl:w-32 xl:h-32 rounded-full z-0">
@@ -44,6 +59,5 @@ export const SkillImg = () => {
     </li>
   ));
 
-  console.log(link);
   return <>{link}</>;
 };
